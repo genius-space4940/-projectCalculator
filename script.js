f712706b31d28adb3c559b2a4a66c842bf7d6e54
Yura Pobizhan <urapobizan6@gmail.com>
2024-12-28 18:23:37 +0200

    do better buttons

--- script.js
+++ script.js
@@ -1,37 +1,24 @@
-let expressionField = document.querySelector('.input-field');
+let inputField = document.querySelector('.input-field');
 
-document.querySelector('.point').addEventListener("click", function() {expressionField.value += "."});
-document.querySelector('.percent').addEventListener("click", function() {expressionField.value += "%"});
-document.querySelector('.multiply-op').addEventListener("click", function() {expressionField.value += "*"});
-document.querySelector('.divide-op').addEventListener("click", function() {expressionField.value += "/"});
-document.querySelector('.plus-op').addEventListener("click", function() {expressionField.value += "+"});
-document.querySelector('.minus-op').addEventListener("click", function() {expressionField.value += "-"});
-document.querySelector('.bracket-left').addEventListener("click", function() {expressionField.value += "("});
-document.querySelector('.bracket-right').addEventListener("click", function() {expressionField.value += ")"});
+document.querySelector('.point').addEventListener("click", function() {inputField.value += "."});
+document.querySelector('.percent').addEventListener("click", function() {inputField.value += "%"});
+document.querySelector('.multiply-op').addEventListener("click", function() {inputField.value += "*"});
+document.querySelector('.divide-op').addEventListener("click", function() {inputField.value += "/"});
+document.querySelector('.plus-op').addEventListener("click", function() {inputField.value += "+"});
+document.querySelector('.minus-op').addEventListener("click", function() {inputField.value += "-"});
+document.querySelector('.bracket-left').addEventListener("click", function() {inputField.value += "("});
+document.querySelector('.bracket-right').addEventListener("click", function() {inputField.value += ")"});
+
 
 for (let i = 0; i < 10; i += 1) {
-    document.querySelector(`.number${i}`).addEventListener('click', function () {expressionField.value += `${i}`});
+    document.querySelector(`.number${i}`).addEventListener('click', function () {inputField.value += `${i}`});
 }
 
 document.querySelector('.clean-field').addEventListener('click', function(){
-    expressionField.value = '';
+    inputField.value = '';
 })
 
 
 document.querySelector('.equal').addEventListener('click', function(){
-    let expression = expressionField.value;
-    if (expression.includes('+')) {
-        let numbers = expression.split('+');
-        expressionField.value = +numbers[0] + +numbers[1];
-        console.log('+')
-    } else if (expression.includes('-')) {
-        let numbers = expression.split('-');
-        expressionField.value = +numbers[0] - +numbers[1];
-    } else if (expression.includes('*')) {
-        let numbers = expression.split('*');
-        expressionField.value = +numbers[0] * +numbers[1];
-    } else if (expression.includes('/')) {
-        let numbers = expression.split('/');
-        expressionField.value = +numbers[0] / +numbers[1];
-    }
+    inputField.value = eval(inputField.value);
 })
