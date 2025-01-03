f58691b7caeb7d24ec7d03ea2216320ff51d5fce
Maintrop4940 <155757346+genius-space4940@users.noreply.github.com>
2025-01-03 22:33:11 +0200

    change button % for ^, operations; add functions;

--- script.js
+++ script.js
@@ -1,24 +1,92 @@
 let inputField = document.querySelector('.input-field');
+let expressionIsEvaluated = false;
 
-document.querySelector('.point').addEventListener("click", function() {inputField.value += "."});
-document.querySelector('.percent').addEventListener("click", function() {inputField.value += "%"});
-document.querySelector('.multiply-op').addEventListener("click", function() {inputField.value += "*"});
-document.querySelector('.divide-op').addEventListener("click", function() {inputField.value += "/"});
-document.querySelector('.plus-op').addEventListener("click", function() {inputField.value += "+"});
-document.querySelector('.minus-op').addEventListener("click", function() {inputField.value += "-"});
-document.querySelector('.bracket-left').addEventListener("click", function() {inputField.value += "("});
-document.querySelector('.bracket-right').addEventListener("click", function() {inputField.value += ")"});
+document.querySelector('.point').addEventListener("click", function() {
+    if (expressionIsEvaluated) {
+        inputField.value = ".";
+        expressionIsEvaluated = false;
+    } else {
+        inputField.value += ".";
+    }
+});
 
+document.querySelector('.exponent').addEventListener("click", function() {
+    addOperator("^");
+});
+
+document.querySelector('.multiply-op').addEventListener("click", function() {
+    addOperator("×");
+});
+
+document.querySelector('.divide-op').addEventListener("click", function() {
+    addOperator("÷");
+});
+
+document.querySelector('.plus-op').addEventListener("click", function() {
+    addOperator("+");
+});
+
+document.querySelector('.minus-op').addEventListener("click", function() {
+    addOperator("-");
+});
+
+document.querySelector('.bracket-left').addEventListener("click", function() {
+    if (expressionIsEvaluated) {
+        inputField.value = "(";
+        expressionIsEvaluated = false;
+    } else {
+        inputField.value += "(";
+    }
+});
+
+document.querySelector('.bracket-right').addEventListener("click", function() {
+    if (expressionIsEvaluated) {
+        inputField.value = ")";
+        expressionIsEvaluated = false;
+    } else {
+        inputField.value += ")";
+    }
+});
 
 for (let i = 0; i < 10; i += 1) {
-    document.querySelector(`.number${i}`).addEventListener('click', function () {inputField.value += `${i}`});
+    document.querySelector(`.number${i}`).addEventListener('click', function () {
+        if (expressionIsEvaluated) {
+            inputField.value = `${i}`;
+            expressionIsEvaluated = false;
+        } else {
+            inputField.value += `${i}`;
+        }
+    });
 }
 
 document.querySelector('.clean-field').addEventListener('click', function(){
     inputField.value = '';
-})
-
+    expressionIsEvaluated = false;
+});
 
 document.querySelector('.equal').addEventListener('click', function(){
-    inputField.value = eval(inputField.value);
-})
+    try {
+        let expression = inputField.value;
+        expression = expression.replace("×", "*").replace("÷", "/");
+        inputField.value = eval(expression);
+        expressionIsEvaluated = true;
+    } catch (error) {
+        inputField.value = "Помилка";
+
+        setTimeout(() => {
+            inputField.value = '';
+        }, 1500);
+    }
+});
+
+function addOperator(operator) {
+    let currentValue = inputField.value;
+
+    if (["+", "-", "×", "÷", "^"].includes(currentValue.slice(-1))) {
+        inputField.value = currentValue.slice(0, -1) + operator;
+    } else {
+        inputField.value += operator;
+    }
+
+    expressionIsEvaluated = false;
+}
